fc96c28597789accae9fab24c8af8150f8ca1243
Babar Ali <babarali36912@gmail.com>
2026-06-29 17:06:35 +0500

    TSK-05: Write Auth & Role Middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+export const verifyJWT = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "Token Missing",
+      });
+    }
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Token missing",
+      });
+    }
+    const decoded = jwt.verify(token, "KEY");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+        message: "Invalid or Expired Token"
+    });
+  }
+};
+
+export const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Acsess Denied: You do not have permission",
+      });
+    }
+    next();
+  };
+};
